92b5bfaf90e424f8c1cb5856d72b0b44eb967a18
LiQiang0307 <2889663102@qq.com>
2022-12-18 12:09:58 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ Descripttion:
 version: 
 Author: LiQiang
 Date: 2022-12-18 11:52:33
-LastEditTime: 2022-12-18 12:05:02
+LastEditTime: 2022-12-18 12:08:33
 '''
 import setuptools
 setuptools.setup(
     name="shanhai",
     description="测试",
+    version="0.1",
     url="https://github.com/ShanHai-AI/shaihai",
     author="ShanHai-AI",
     author_email="2889663102@qq.com",
