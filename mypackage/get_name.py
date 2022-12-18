aa0ecac8b50c983998b76d73722637ca22b34df5
LiQiang0307 <2889663102@qq.com>
2022-12-18 12:03:51 +0800

    Add files via upload

---
+++ mypackage/get_name.py
@@ -0,0 +1,12 @@
+'''
+Descripttion: 
+version: 
+Author: LiQiang
+Date: 2022-12-18 11:51:39
+LastEditTime: 2022-12-18 11:59:52
+'''
+def get_name(name):
+    return "我是"+name
+
+if __name__ == '__main__':
+    print(get_name("小明"))
