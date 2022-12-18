f782089c432eca9a737f9eb65ac71881d305fafa
LiQiang0307 <2889663102@qq.com>
2022-12-18 12:03:20 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,12 @@
+'''
+Descripttion: 
+version: 
+Author: LiQiang
+Date: 2022-12-18 11:50:02
+LastEditTime: 2022-12-18 11:50:27
+'''
+def hello():
+    print("Hello World!")
+
+if __name__ == '__main__':
+    hello()
---
+++ setup.py
@@ -0,0 +1,17 @@
+'''
+Descripttion: 
+version: 
+Author: LiQiang
+Date: 2022-12-18 11:52:33
+LastEditTime: 2022-12-18 12:01:18
+'''
+import setuptools
+setuptools.setup(
+    name="shanhai",
+    description="测试",
+    author="ShanHai-AI",
+    author_email="2889663102@qq.com",
+    include_package_data=True,
+    platforms="any",
+    package=setuptools.find_packages()
+)
